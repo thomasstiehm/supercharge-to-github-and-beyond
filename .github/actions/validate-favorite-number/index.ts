b0100402deadb15d00f67a4becc74784e6e57901
Phil <coveros-phil@github.com>
2023-09-11 14:01:38 -0400

    Started working on the custom action to verify that the favorite number is good

---
+++ .github/actions/validate-favorite-number/index.ts
@@ -0,0 +1,10 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+
+export async function run() {
+    core.debug('Starting validate-favorite-number action');
+}
+
+
+run();
