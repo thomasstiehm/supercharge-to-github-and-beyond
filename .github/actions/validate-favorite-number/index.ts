be1198bd8f90c2210f2d65be1b96631d7193d7d3
Phil <coveros-phil@github.com>
2023-09-11 14:22:50 -0400

    Work

--- .github/actions/validate-favorite-number/index.ts
+++ .github/actions/validate-favorite-number/index.ts
@@ -2,8 +2,14 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 
+
 export async function run() {
     core.debug('Starting validate-favorite-number action');
+
+    const eventJson = core.getInput('event_ctx');
+    core.info(eventJson);
+    const event = JSON.parse(eventJson);
+    core.info(event);
 }
 
 
